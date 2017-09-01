e557db7604f5795b92069bae95e53b3e62bb1707
martynST <31521568+martynST@users.noreply.github.com>
2017-09-01 01:33:36 +0100

    Delete Student.java

--- Student.java
+++
@@ -1,6 +0,0 @@
-public class Student extends Ticket{
-    public Student(String filmName, String showingDay, Time showingTime)
-    {//
-        super("Student",6.00, filmName, showingDay, showingTime);
-    }
-}
