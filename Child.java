dcfc7dda2cb80b2ae1f94df3065c737d28d1624d
martynST <31521568+martynST@users.noreply.github.com>
2017-09-01 01:32:59 +0100

    Delete Child.java

--- Child.java
+++
@@ -1,6 +0,0 @@
-public class Child extends Ticket{
-    public Child(String filmName, String showingDay, Time showingTime)
-    {
-        super("Child",4.00, filmName, showingDay, showingTime);
-    }
-}
